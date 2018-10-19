506f3eee61e4c9afe4e985bfb8c092d40e98848e
James Keaveney <james.keaveney@desy.de>
2018-10-19 18:35:11 +0200

    few improvments towards n-D scan

--- example.py
+++ example.py
@@ -2,6 +2,9 @@ from tools import splash
 from tools import scanner
 from asciimatics.screen import Screen
 import numpy as np
+import itertools
+import matplotlib as plt
+
 
 #intro graphics
 Screen.wrapper(splash.deft_splash)
@@ -9,20 +12,44 @@ Screen.wrapper(splash.deft_splash)
 data = [1.0, 2.0, 3.0]
 invcov = [[1.5, -0.49, -0.5], [-0.49, 1.5,  -0.5], [-0.50, -0.5,1.5]]
 
-#configure scan
-coefficients = ["ctg"]
+max_coeff_power = 1
 
-pred_basis = {'SM': np.array([2.0,3.0,4.0]), 'ctg-': np.array([1.5,2.5,3.5]), 'ctg+': np.array([2.5,3.5,4.5]) }
+#configure scan
+coefficients = ["ctg", "ctw"]
+
+preds = {'SM': np.array([2.0,3.0,4.0]),
+              'ctg-': np.array([1.5,2.5,3.5]),
+              'ctg+': np.array([2.5,3.5,4.5]),
+              'ctw-': np.array([1.2,2.0,2.4]),
+              'ctw+': np.array([2.1,3.2,4.1])}
 
 sc  = scanner.scanner()
-sc.init(data,invcov,pred_basis)
+sc.init(data,invcov,preds,coefficients,max_coeff_power)
 
-scan_params = [-1.0, 1.0, 21]
+scan_params = {
+        'ctg': np.linspace(-1.0, 1.0, 5),
+        'ctw': np.linspace(-1.0, 1.0, 3),
+}
 
-paramspace = np.linspace(scan_params[0],scan_params[1],scan_params[2])
+ranges = [ ]
 
-for scenario in paramspace:
-    chi_sq = sc.eval(scenario)
-    print "scenario = " + str(scenario) + "chi sqared equals = " + str(chi_sq)
+for coefficient in coefficients:
+    ranges.append(scan_params[coefficient].tolist())
+
+chi_sq_array = np.array()
+
+# build parameter space
+for scenario in itertools.product(*ranges):
+    chi_sq_array.append(sc.eval(scenario))
+    #print "scenario = " + str(scenario) + "chi sqared equals = " + str(chi_sq)
+
+
+chi_sq_array.reshape(len(coefficients),len(coefficients) )
+
+plt.contourf(scan_params['ctg'], scan_params['ctw'], chi_sq_array, 8, alpha=.75, cmap='jet')
+
+
+#C = plt.contour(X, Y, f(X, Y), 8, colors='black', linewidth=.5)
+
 
 
--- tools/scanner.py
+++ tools/scanner.py
@@ -1,17 +1,37 @@
+import numpy as np
+
 class scanner():
-    def init(self,data,invcov,pred_basis):
-        print "initialising scanner"
+    def init(self,data,invcov,preds,coefficients,max_coeff_power):
+        #print "initialising scanner"
         self.data = data
         self.invcov = invcov
-        self.pred_basis = pred_basis
+        self.preds = preds
+        self.coefficients = coefficients
+        self.max_coeff_power = max_coeff_power
         
-    def eval(self, scenario):
-        print "evaluating objective function" + str(scenario)
-        
-        pred =  (self.pred_basis['SM']) + (scenario*self.pred_basis['ctg-'])
+        pred_basis = {}
     
+        #isolate SM--c_i term
+        # SM--c_i interference term
+        for c in coefficients:
+            ci_m_name = c + "-"
+            ci_p_name = c + "+"
+            sigma_sm_ci = np.subtract(preds[ci_p_name], preds[ci_m_name])
+            basis_c_sm_name = c + "_sm"
+            pred_basis[basis_c_sm_name] = sigma_sm_ci
+        self.pred_basis = pred_basis
+
+    def eval(self, scenario,):
+        pred =  self.preds['SM']
+        for c in range(0, len(self.coefficients)):
+            basis_c_sm_name = self.coefficients[c] + "_sm"
+            pred += (scenario[c]*self.pred_basis[basis_c_sm_name])
+        
         for datum_i in range(0, len(self.data)):
             for datum_j in range(0, len(self.data)):
                 chi_sq = (self.data[datum_i] - pred[datum_j])*(self.data[datum_i] - pred[datum_j])*(self.invcov[datum_i][datum_j])
+        
+        print "evalutaing point  ------   " + str(self.coefficients[0]) + " = " + str(scenario[0]) + " " + str(self.coefficients[1]) + " = " + str(scenario[1]) + " chi2 = " + str(chi_sq)
+
     
         return chi_sq
