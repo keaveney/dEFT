5a2682559c602aeab44e90074dd90f872e29f4e7
James Keaveney <james.keaveney@desy.de>
2018-10-23 16:40:34 +0200

    working N-D fits

--- test_emcee.py
+++ test_emcee.py
@@ -1,43 +1,12 @@
-from tools import splash
-from tools import scanner
-from asciimatics.screen import Screen
 import numpy as np
-import itertools
-import matplotlib.pyplot as pl
-import corner
 import emcee
 import time
 
-#intro graphics
-Screen.wrapper(splash.deft_splash)
-
-#start time
 start = time.time()
 
-######################################################
-###############   DEFINE THE DATA   ##################
-######################################################
-#bins
-bins = np.array([-1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5])
+bins = [-1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5]
 
-#data
-data = np.array([5.0, 5.0, 3.0, 3.0, 3.0, 3.0, 3.0])
-
-#covariance matrix and inverse
-cov = [[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
-icov = np.linalg.inv(cov)
-
-######################################################
-###############   DEFINE THE MODEL   #################
-######################################################
-
-#list of coeffieicnts/operators to be considered
 coefficients = ["ctg", "ctw", "ctphi", "ctb", "cphit", "cphiQ1", "cphiQ3"]
-
-#the tricky part - list of predictions for data and for +/- X for each coefficient
-# if max_coeff_power = 1, and more than one operator is being considred,
-# this dictonary also needs predictions for (ci,cj) = (X,X)
-# for all pairs of operators
 
 preds = {'SM': np.array([3.0,3.0,3.0, 3.0,3.0,3.0, 3.0]),
     'ctg-': np.array([2.0,3.0,3.0,3.0,3.0,3.0,3.0]),
@@ -55,42 +24,67 @@ preds = {'SM': np.array([3.0,3.0,3.0, 3.0,3.0,3.0, 3.0]),
     'cphiQ3-': np.array([3.0,3.0,3.0,3.0,3.0,3.0,2.0]),
     'cphiQ3+': np.array([3.0,3.0,3.0,3.0,3.0,3.0,4.0])
 }
-#maximum power of c_i/Lambda^2 to be considered
-# this can only be 1 or 2
-# 1: Lambda^{-2} - interference between diagrams with single dim6 operator and SM diagrams
-# 2: Lambda^{-2} + Lambda^{-4} - the above with the 'squared' terms (diagrams with single dim6 operator squared)
-#                                  and 'cross' terms (interefernece between pairs of diagrams with single dim6 operator each)
 
-max_coeff_power = 1
+data = [5.0, 5.0, 3.0, 3.0, 3.0, 3.0, 3.0]
+#icov = [[1.5, -0.49, -0.5], [-0.49, 1.5,  -0.5], [-0.50, -0.5,1.5]]
+cov = [[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
+icov = np.linalg.inv(cov)
+
+pred_basis = {}
+        
+#isolate SM--c_i term
+# SM--c_i interference term
 
 
-######################################################
-###############   DEFINE THE FIT/SCAN   ##############
-######################################################
-sc  = scanner.scanner()
-sc.init(data,icov,preds,coefficients,max_coeff_power)
+def make_pred(c):
+    pred = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    for ci in range(0, len(coefficients)):
+        basis_c_sm_name = coefficients[ci] + "_sm"
+        linear_contrib = (c[ci]*pred_basis[basis_c_sm_name])
+        pred += linear_contrib
+    pred = pred + preds['SM']
+    return pred
 
-nwalkers = 100
+print "SM pred is  = " + str(make_pred([0.0,0.0,0.0,0.0,0.0,0.0,0.0]))
+
+def lnprob(c, data, invcov):
+    pred = make_pred(c)
+    diff = pred - data
+    #print" c = " +str(c) + " pred = " + str(pred) + " data = " + str(data) + " diff = " + str(diff) + " ll = " + str(-np.dot(diff,np.dot(invcov,diff))/2.0)
+    return -np.dot(diff,np.dot(invcov,diff))/2.0
+
 ndim = 7
 
+nwalkers = 200
 
 p0 = np.random.rand(ndim * nwalkers).reshape((nwalkers, ndim))
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, scanner.lnprob, args=[data, icov])
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[data, icov])
 
 pos, prob, state = sampler.run_mcmc(p0, 100)
 
+#print "p0 = " + str(p0) + " pos = " + str(pos)
+
 sampler.reset()
-
 sampler.run_mcmc(pos, 500)
-
 samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
 
+import matplotlib.pyplot as pl
 
-######################################################
-###############   MAKE RESULTS PLOTS  ################
-######################################################
+#for i in range(ndim):
+# pl.figure()
+#pl.hist(sampler.flatchain[:,i], 300, color="k", histtype="step")
+#pl.title("Dimension {0:d}".format(i))
+#pl.show()
+
+# red dashes, blue squares and green triangles
+#plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+#pl.plot(bins, data, 'ro')
+#pl.errorbar(bins, data, 'ro', xerr=0.2, yerr=0.4)
+
 pl.figure()
+
+print "len xs = " + str(len(bins)) +  " len ys = " + str(make_pred([1.0,0.0,0.0,0.0,0.0,0.0,0.0]))
 
 pl.errorbar(bins, make_pred([1.0,0.0,0.0,0.0,0.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='ctg=1.0')
 pl.errorbar(bins, make_pred([0.0,1.0,0.0,0.0,0.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='ctw=1.0')
@@ -103,16 +97,16 @@ pl.errorbar(bins, preds['SM'], xerr=0.0, yerr=0.0, label='SM')
 pl.errorbar(bins, data, fmt="o",xerr=0.25, yerr=0.05, label='Data')
 
 pl.axis([-1.0, 1.0, -5.0, 10.0])
+
 pl.legend()
 pl.show()
 
+import corner
 fig = corner.corner(samples, labels=["$ctg$", "$ctw$", "$ctphi$", "$ctb$", "$cphit$","$ctphiQ1$","$ctphiQ3$"],
                     truths=[0.0, 0.0, 0.0,0.0, 0.0, 0.0,0.0])
 
 fig.show()
 fig.savefig("triangle.png")
 
-
-#end time
 end = time.time()
 print "Total elapsed wall time  = " +  str(end - start)
