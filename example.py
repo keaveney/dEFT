cc6dc11778937684f14dbd14f35b349096363d09
James Keaveney <james.keaveney@desy.de>
2018-10-24 17:28:42 +0200

    first guess at inlcusion of all Lambda^{-4} terms in toy predictions... to be re-checked

--- example.py
+++ example.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as pl
 import corner
 import emcee
 import time
+import json
+
 
 #intro graphics
 Screen.wrapper(splash.deft_splash)
@@ -19,12 +21,13 @@ start = time.time()
 ######################################################
 #bins
 bins = np.array([-1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5])
+nbins = len(bins)
 
 #data
-data = np.array([5.0, 5.0, 3.0, 3.0, 3.0, 3.0, 3.0])
+data = (4.0)*(np.array([7.0, 7.0, 3.0, 3.0, 3.0, 7.0, 7.0]))
 
 #covariance matrix and inverse
-cov = [[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
+cov = [[1.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0], [0.7, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0], [0.0, 0.5, 1.0, 0.5, 0.0, 0.0, 0.0], [0.0, 0.0, 0.5, 1.0, 0.5, 0.0, 0.0],[0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0],[0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5], [0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0]]
 icov = np.linalg.inv(cov)
 
 ######################################################
@@ -33,92 +36,182 @@ icov = np.linalg.inv(cov)
 
 #list of coeffieicnts/operators to be considered
 coefficients = ["ctg", "ctw", "ctphi", "ctb", "cphit", "cphiQ1", "cphiQ3"]
+ndim = len(coefficients)
+
+
+preds =  {
+                'SM': np.array([3.0,3.0,3.0, 3.0,3.0,3.0, 3.0]),
+                'ctg-': np.array([2.0,3.0,3.0,3.0,3.0,3.0,3.0]),
+                'ctg+': np.array([4.0,3.0,3.0,3.0,3.0,3.0,3.0]),
+                'ctw-': np.array([3.0,2.0,3.0,3.0,3.0,3.0,3.0]),
+                'ctw+': np.array([3.0,4.0,3.0,3.0,3.0,3.0,3.0]),
+                'ctphi-': np.array([3.0,3.0,2.0,3.0,3.0,3.0,3.0]),
+                'ctphi+': np.array([3.0,3.0,4.0,3.0,3.0,3.0,3.0]),
+                'ctb-': np.array([3.0,3.0,3.0,2.0,3.0,3.0,3.0]),
+                'ctb+': np.array([3.0,3.0,3.0,4.0,3.0,3.0,3.0]),
+                'cphit-': np.array([3.0,3.0,3.0,3.0,2.0,3.0,3.0]),
+                'cphit+': np.array([3.0,3.0,3.0,3.0,4.0,3.0,3.0]),
+                'cphiQ1-': np.array([3.0,3.0,3.0,3.0,3.0,2.0,3.0]),
+                'cphiQ1+': np.array([3.0,3.0,3.0,3.0,3.0,4.0,3.0]),
+                'cphiQ3-': np.array([3.0,3.0,3.0,3.0,3.0,3.0,2.0]),
+                'cphiQ3+': np.array([3.0,3.0,3.0,3.0,3.0,3.0,4.0])
+}
+
+
+#just generate some dummy cross predictions
+for c in coefficients:
+    ci_m_name = c + "-"
+    ci_p_name = c + "+"
+    for d in coefficients:
+        if c != d:
+            cj_m_name = c + "-"
+            cj_p_name = c + "+"
+            ci_cj_m_name = c + "-" + d + "-"
+            ci_cj_p_name = c + "+" + d + "+"
+            #wild guess
+            preds[ci_cj_m_name] = preds[ci_m_name] + preds[cj_m_name] - (0.8*preds['SM'])
+            preds[ci_cj_p_name] = preds[ci_p_name] + preds[cj_p_name] - (0.8*preds['SM'])
+
+
+priors =  {
+        'ctg': np.array([-3.0,3.0]),
+        'ctw': np.array([-3.0,3.0]),
+        'ctphi': np.array([-3.0,3.0]),
+        'ctb': np.array([-3.0,3.0]),
+        'cphit': np.array([-3.0,3.0]),
+        'cphiQ1': np.array([-3.0,3.0]),
+        'cphiQ3': np.array([-3.0,3.0])
+}
 
 #maximum power of c_i/Lambda^2 to be considered
 # this can only be 1 or 2
 # 1: Lambda^{-2} - interference between diagrams with single dim6 operator and SM diagrams
 # 2: Lambda^{-2} + Lambda^{-4} - the above with the 'squared' terms (diagrams with single dim6 operator squared)
 #                                  and 'cross' terms (interefernece between pairs of diagrams with single dim6 operator each)
-max_coeff_power = 1
+max_coeff_power = 2
 
 #the tricky part - list of predictions for data and for +/- X for each coefficient
 # if max_coeff_power = 1, and more than one operator is being considred,
 # this dictonary also needs predictions for (ci,cj) = (X,X)
 # for all pairs of operators
 
-preds = {
-    'SM': np.array([3.0,3.0,3.0, 3.0,3.0,3.0, 3.0]),
-    'ctg-': np.array([2.0,3.0,3.0,3.0,3.0,3.0,3.0]),
-    'ctg+': np.array([4.0,3.0,3.0,3.0,3.0,3.0,3.0]),
-    'ctw-': np.array([3.0,2.0,3.0,3.0,3.0,3.0,3.0]),
-    'ctw+': np.array([3.0,4.0,3.0,3.0,3.0,3.0,3.0]),
-    'ctphi-': np.array([3.0,3.0,2.0,3.0,3.0,3.0,3.0]),
-    'ctphi+': np.array([3.0,3.0,4.0,3.0,3.0,3.0,3.0]),
-    'ctb-': np.array([3.0,3.0,3.0,2.0,3.0,3.0,3.0]),
-    'ctb+': np.array([3.0,3.0,3.0,4.0,3.0,3.0,3.0]),
-    'cphit-': np.array([3.0,3.0,3.0,3.0,2.0,3.0,3.0]),
-    'cphit+': np.array([3.0,3.0,3.0,3.0,4.0,3.0,3.0]),
-    'cphiQ1-': np.array([3.0,3.0,3.0,3.0,3.0,2.0,3.0]),
-    'cphiQ1+': np.array([3.0,3.0,3.0,3.0,3.0,4.0,3.0]),
-    'cphiQ3-': np.array([3.0,3.0,3.0,3.0,3.0,3.0,2.0]),
-    'cphiQ3+': np.array([3.0,3.0,3.0,3.0,3.0,3.0,4.0])
-}
+# for now assume all input predictions correspond to c_i = +/- 2.0
+c_i_benchmark = 2.0
 
-#define prediction builder function for convenience
-pb = pred_builder.pred_builder()
-pred_basis = pb.make_basis(preds,coefficients,max_coeff_power)
+
+def make_basis(preds,coefficients,max_coeff_power):
+    pred_basis = {}
+    for c in coefficients:
+        ci_m_name = c + "-"
+        ci_p_name = c + "+"
+        # sm--ci interference contribution
+        sigma_sm_ci = (np.subtract(preds[ci_p_name], preds[ci_m_name]) / (2*c_i_benchmark))
+        # ci--ci squared contribution
+        if max_coeff_power > 1.0:
+            sigma_ci_ci = (np.add(preds[ci_p_name], preds[ci_m_name]) - (2.0*preds['SM']) ) / (2*(c_i_benchmark**2))
+            for d in coefficients:
+                # TODO ci--cj interference contribution
+                if c != d:
+                    ci_cj_m_name = c + "-" + d + "-"
+                    ci_cj_p_name = c + "+" + d + "+"
+                    if max_coeff_power > 1.0:
+                        sigma_cj_cj = (np.add(preds[ci_cj_m_name], preds[ci_cj_p_name]) - (2.0*preds['SM']) ) / (2*(c_i_benchmark**2))
+                        sigma_ci_cj = (np.add(preds[ci_cj_m_name], preds[ci_cj_p_name]) - (2.0*preds['SM'])  - (2.0*(sigma_ci_ci**2)) - (2.0*(sigma_cj_cj**2)) ) / (2*(c_i_benchmark**2))
+                    else:
+                        sigma_ci_ci = np.zeros(len(preds['SM']))
+                    basis_ci_cj_name = c + "_" + d
+                    pred_basis[basis_ci_cj_name] = sigma_ci_cj
+        else:
+            sigma_ci_ci = np.zeros(len(preds['SM']))
+        
+        basis_ci_sm_name = c + "_sm"
+        basis_ci_ci_name = c + "_" + c
+
+        pred_basis[basis_ci_sm_name] = sigma_sm_ci
+        pred_basis[basis_ci_ci_name] = sigma_ci_ci
+
+    return pred_basis
+
+pred_basis = make_basis(preds,coefficients,max_coeff_power)
+
+def make_pred(c):
+    pred = np.zeros(nbins)
+    for ci in range(0, len(coefficients)):
+        basis_ci_sm_name = coefficients[ci] + "_sm"
+        basis_ci_ci_name = coefficients[ci] + "_" + coefficients[ci]
+
+        ci_sm_contrib = (c[ci]*pred_basis[basis_ci_sm_name])
+        ci_ci_contrib = ((c[ci]**2)*pred_basis[basis_ci_ci_name])
+
+        pred += ci_sm_contrib
+        pred += ci_ci_contrib
+                       
+    pred = pred + preds['SM']
+    return pred
+
+def lnprior(c):
+    #one could play with implementing non-flat priors here
+    lnp = 0.0
+    for c in coefficients:
+        if ((c < priors[c][0]) | (c > priors[c][1])):
+            prob = -np.inf
+    return lnp
+
+def lnprob(c, data, icov):
+    pred = make_pred(c)
+    diff = pred - data
+    return (-np.dot(diff,np.dot(icov,diff))/2.0) + lnprior(c)
 
 ######################################################
 ###############   DEFINE THE FIT/SCAN   ##############
 ######################################################
+nWalkers = 100
+ndim = 7
+nBurnIn = 50
+nTotal = 300
 
-#define define the objective function that will be minimised/maximised
-def lnprob(c, data, icov):
-    pred = pb.make_pred(c)
-    diff = pred - data
-    #print" c = " +str(c) + " pred = " + str(pred) + " data = " + str(data) + " diff = " + str(diff) + " ll = " + str(-np.dot(diff,np.dot(invcov,diff))/2.0)
-    return -np.dot(diff,np.dot(icov,diff))/2.0
-
-#define define the objective function that will be minimised/maximiseds
-nwalkers = 100
-ndim = len(coefficients)
-
-p0 = np.random.rand(ndim * nwalkers).reshape((nwalkers, ndim))
-
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[data, icov])
-
-pos, prob, state = sampler.run_mcmc(p0, 100)
-
+p0 = np.random.rand(ndim * nWalkers).reshape((nWalkers, ndim))
+sampler = emcee.EnsembleSampler(nWalkers, ndim, lnprob, args=[data, icov])
+pos, prob, state = sampler.run_mcmc(p0, nBurnIn)
 sampler.reset()
+sampler.run_mcmc(pos,nTotal)
+samples = sampler.chain.reshape((-1, ndim))
 
-sampler.run_mcmc(pos, 500)
+mcmc_params = np.mean(sampler.flatchain,axis=0)
+mcmc_params_cov = np.cov(np.transpose(sampler.flatchain))
 
-samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
-
+print "best fit vals = " + str(mcmc_params)
+print "uncertainties = " + str(np.sqrt(np.diag(mcmc_params_cov)))
 
 ######################################################
 ###############   MAKE RESULTS PLOTS  ################
 ######################################################
+# raw predictions and data
 pl.figure()
-
-pl.errorbar(bins, pb.make_pred([1.0,0.0,0.0,0.0,0.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='ctg=1.0')
-pl.errorbar(bins, pb.make_pred([0.0,1.0,0.0,0.0,0.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='ctw=1.0')
-pl.errorbar(bins, pb.make_pred([0.0,0.0,1.0,0.0,0.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='ctphi=1.0')
-pl.errorbar(bins, pb.make_pred([0.0,0.0,0.0,1.0,0.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='ctb=1.0')
-pl.errorbar(bins, pb.make_pred([0.0,0.0,0.0,0.0,1.0,0.0,0.0]), xerr=0.0, yerr=0.0, label='cphit=1.0')
-pl.errorbar(bins, pb.make_pred([0.0,0.0,0.0,0.0,0.0,1.0,0.0]), xerr=0.0, yerr=0.0, label='ctphiQ1=1.0')
-pl.errorbar(bins, pb.make_pred([0.0,0.0,0.0,0.0,0.0,0.0,1.0]), xerr=0.0, yerr=0.0, label='ctphiQ3=1.0')
+for c in range(0,len(coefficients)):
+    vals = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    vals[c] = 1.0
+    label_string = coefficients[c] + "=1.0"
+    pl.errorbar(bins, make_pred(vals), xerr=0.0, yerr=0.0, label=label_string)
 pl.errorbar(bins, preds['SM'], xerr=0.0, yerr=0.0, label='SM')
 pl.errorbar(bins, data, fmt="o",xerr=0.25, yerr=0.05, label='Data')
-
-pl.axis([-1.0, 1.0, -5.0, 10.0])
+pl.axis([bins[0]-0.25, bins[nbins-1]+0.25, -5.0, 10.0])
 pl.legend()
-pl.show()
+#pl.show()
 
-fig = corner.corner(samples, labels=["$ctg$", "$ctw$", "$ctphi$", "$ctb$", "$cphit$","$ctphiQ1$","$ctphiQ3$"],
-                    truths=[0.0, 0.0, 0.0,0.0, 0.0, 0.0,0.0])
+#best fit prediction and data
+#TODO
 
-fig.show()
+
+#corner plot
+#fig = corner.corner(samples, labels=["$ctg$", "$ctw$", "$ctphi$", "$ctb$", "$cphit$","$ctphiQ1$","$ctphiQ3$"],
+#                    truths=[0.0, 0.0, 0.0,0.0, 0.0, 0.0,0.0])
+
+fig = corner.corner(samples, labels=["$ctg$", "$ctw$", "$ctphi$", "$ctb$", "$cphit$", "$ctphiQ1$", "$ctphiQ3$"],
+                    quantiles=[0.05, 0.95],
+                       range=[1.0,1.0,1.0,1.0,1.0,1.0,1.0], truths=[0.0, 0.0, 0.0,0.0, 0.0, 0.0,0.0],
+                       show_titles=True, title_kwargs={"fontsize": 18})
+
 fig.savefig("triangle.png")
 
 
